05413f5880cf3641d967cf173881aa3d0a78bfd8
Siratee K <sirateek@gmail.com>
2022-06-09 14:28:19 +0700

    feat: Add reply feature for new connection

--- websocket-hub/client.go
+++ websocket-hub/client.go
@@ -25,35 +25,42 @@ var wsUpgrader = websocket.Upgrader{
 type WebSocketClient interface {
 	HandleSocket()
 	SendMessage(s []byte)
+	setStatus()
+	CloseConnection()
 }
 
 type webSocketClient struct {
 	ginContext          *gin.Context
 	webSocketConnection *websocket.Conn
 	status              WebSocketStatus
+	ClientId            string
+	wscHub              *wsConnectionHub
 }
 
-func NewWebSocketClient(c *gin.Context) *webSocketClient {
+func NewWebSocketClient(c *gin.Context, id string, wscHub *wsConnectionHub) *webSocketClient {
 
 	ws, _ := wsUpgrader.Upgrade(c.Writer, c.Request, nil)
 	wsc := &webSocketClient{
 		ginContext:          c,
 		webSocketConnection: ws,
 		status:              INIT,
+		ClientId:            id,
+		wscHub:              wscHub,
 	}
+	fmt.Println("New connection with ID ", id)
 	// Handle Each client in each thread.
 	go wsc.HandleSocket()
-
-	wsc.status = CONNECTED
+	wsc.setStatus(CONNECTED)
 
 	return wsc
 }
 
 func (wsc *webSocketClient) HandleSocket() {
-	defer wsc.webSocketConnection.Close()
+	defer wsc.CloseConnection()
 	for {
 		_, message, err := wsc.webSocketConnection.ReadMessage()
 		if message != nil {
+			// Broadcast incomming message
 			fmt.Println(string(message))
 		}
 		if err != nil {
@@ -62,13 +69,24 @@ func (wsc *webSocketClient) HandleSocket() {
 		}
 		// wsc.webSocketConnection.WriteMessage(mt, message)
 	}
-	wsc.status = CLOSED
+}
+
+func (wsc *webSocketClient) CloseConnection() {
+	wsc.webSocketConnection.Close()
+	wsc.setStatus(CLOSED)
+}
+
+func (wsc *webSocketClient) setStatus(webSocketStatus WebSocketStatus) {
+	fmt.Println("Client ", wsc.ClientId, " status has been updated to", webSocketStatus)
+	wsc.status = webSocketStatus
+	// Notify the observer
+	wsc.wscHub.updateClientState(wsc.ClientId, webSocketStatus)
 }
 
 func (wsc *webSocketClient) SendMessage(message []byte) {
 	if wsc.status != CONNECTED {
 		return
 	}
-	fmt.Println("Sending message")
+	fmt.Println("Sending message to client ", wsc.ClientId)
 	wsc.webSocketConnection.WriteMessage(1, message)
 }
--- websocket-hub/hub.go
+++ websocket-hub/hub.go
@@ -1,37 +1,55 @@
 package websockethub
 
 import (
+	"fmt"
+	requestmanager "open-api-inspector-backend/request-manager"
+
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type WSConnectionHub interface {
 	AddClient(c *gin.Context)
-	RemoveClient()
+	RemoveClient(clientId string)
 	Broadcast()
+	updateClientState(clientId string, status WebSocketStatus)
 }
 
 type wsConnectionHub struct {
-	clients []webSocketClient
+	clients map[string]webSocketClient
 }
 
 func NewWsConnectionHub() *wsConnectionHub {
 	return &wsConnectionHub{
-		clients: nil,
+		clients: make(map[string]webSocketClient, 0),
 	}
 }
 
-func (wsc *wsConnectionHub) AddClient(c *gin.Context) {
-	webSocketClient := NewWebSocketClient(c)
-	wsc.clients = append(wsc.clients, *webSocketClient)
+func (wsc *wsConnectionHub) AddClient(c *gin.Context, apiRequests map[string]requestmanager.ApiRequest) {
+	clientId := uuid.New().String()
+	webSocketClient := NewWebSocketClient(c, clientId, wsc)
+	// Send old request to the new client
+	message := []byte("Test")
+	for _, request := range apiRequests {
+		fmt.Println(request)
+		webSocketClient.SendMessage(message)
+	}
+	wsc.clients[clientId] = *webSocketClient
 }
 
-func (wsc *wsConnectionHub) RemoveClient() {
-	// TODO: Handle This
+func (wsc *wsConnectionHub) RemoveClient(clientId string) {
+	// TODO: Handle this
+}
+
+func (wsc *wsConnectionHub) updateClientState(clientId string, status WebSocketStatus) {
+	if status == CLOSED {
+		delete(wsc.clients, clientId)
+	}
 }
 
 func (wsc *wsConnectionHub) Broadcast() {
 	message := []byte("Hello!")
-	for _, client := range wsc.clients {
-		client.SendMessage(message)
+	for _, element := range wsc.clients {
+		element.SendMessage(message)
 	}
 }
